fe0906ffe391103dcdffb8bde3f0b9a6a6288f75
poeynus <poeynus@gmail.com>
2024-04-01 13:39:09 +0900

    진짜 마지막 딱 좋은 상태태

--- src/ModelObj.js
+++ src/ModelObj.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
 
 import React, { useState } from 'react';
 import { useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
 
 function ModelObj() {
   const [dae, setDae] = useState();
-  const colladaData = useLoader(ColladaLoader, './models/d.dae');
+  // const gltfData = useGLTF('./models/d.glb');
+  const colladaData = useLoader(ColladaLoader, './models/f.dae');
 
   // 모델 로드 후 실행
   useState(() => {
@@ -19,8 +21,8 @@ function ModelObj() {
 
   return (
     <>
-      <ambientLight intensity={1} />
-      <pointLight position={[0, 0, 0]} intensity={1} />
+      <ambientLight intensity={2.5} />
+      <pointLight position={[0, 0, 0]} intensity={2} />
       <group>
         <primitive object={dae} position={[-20, -1, 20]} />
       </group>
