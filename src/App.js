f8767b2aac4615e6fceb4a922efbe39c92117264
poeynus <poeynus@gmail.com>
2024-04-02 08:50:49 +0900

    material-depth 구분으로 아이템 표현 성공

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@
 /* eslint-disable react/no-unknown-property */
 import './App.css';
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
 import ModelObj from './ModelObj';
 
 function App() {
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <Canvas>
-        <ModelObj />
-        <axesHelper scale={10} />
-      </Canvas>
+      <ModelObj />
     </div>
   );
 }
--- src/LastModelObj.js
+++ src/LastModelObj.js
@@ -1,35 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoader, Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
-import { MeshStandardMaterial } from 'three';
 
 function ModelObj() {
+  const [dae, setDae] = useState();
   const colladaData = useLoader(ColladaLoader, './models/f.dae');
 
-  // 재렌더링 시 매번 같은 머티리얼을 사용하여 material-depthTest 속성을 적용
-  const material = React.useMemo(
-    () => new MeshStandardMaterial({ color: 'red', depthTest: false }),
-    [],
-  );
+  // 모델 로드 후 실행
+  useState(() => {
+    if (colladaData) {
+      const { scene } = colladaData;
+      setDae(scene);
+    }
+  }, [colladaData]);
 
   return (
     <Canvas camera={{ fov: 75, position: [-10, 20, 20] }}>
       <axesHelper scale={10} />
       <ambientLight intensity={2.5} />
       <pointLight position={[0, 0, 0]} intensity={2} />
-      {colladaData && (
-        <group renderOrder={1}>
-          <primitive object={colladaData.scene} position={[-20, -1, 20]} />
-        </group>
-      )}
+      <group renderOrder={1}>
+        <primitive object={dae} position={[-20, -1, 20]} />
+      </group>
       <mesh
         position={[0, 1, 0]}
         scale={1}
         renderOrder={0}
-        material={material} // 새로 생성한 머티리얼을 적용
+        material-depthTest={false}
       >
         <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="red" />
       </mesh>
       <OrbitControls />
     </Canvas>
