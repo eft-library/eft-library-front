30048eb62aead93d1623ff65c2a22db42d86da50
poeynus <poeynus@gmail.com>
2024-04-03 16:46:04 +0900

    item filter 개발 중

--- src/hooks/useItemFilter.js
+++ src/hooks/useItemFilter.js
@@ -5,18 +5,25 @@ import { useState } from 'react';
 import { ItemList } from 'src/utils/itemConstants';
 
 export const useItemFilter = () => {
-  // root, child value 전부 꺼내서 리스트로 만들기
+  // root, child value 리스트
   const itemValues = ItemList.reduce((acc, item) => {
     acc.push(item.value);
     item.child.forEach((child) => acc.push(child.value));
     return acc;
   }, []);
 
-  // root value만 리스트로 만들기
+  // root value 리스트
   const itemRootValues = ItemList.map((item) => item.value);
+
+  // child value 리스트
+  const itemChildValues = ItemList.map((item) =>
+    item.child.map((childItem) => childItem.value),
+  ).flat();
 
   console.log(itemValues);
   console.log(itemRootValues);
+  console.log(itemChildValues);
+
   const [viewItemList, setViewItemList] = useState(itemValues);
 
   // 클릭시 생기거나 사라짐
@@ -24,13 +31,27 @@ export const useItemFilter = () => {
     // 클릭한 것이 상위 값일 경우 - 전부 없거나, 1개라도 없을 경우 전체 추가 || 상위에 전부 있을 경우 전부 제거
     // 클릭한 것이 이미 있을 경우 - 제거
     // 클릭한 것이 없을 경우 - 추가
+    // if (viewItemList.includes(itemValue)) {
+    //   setViewItemList(viewItemList.filter((item) => item !== itemValue));
+    // } else {
+    //   setViewItemList([...viewItemList, itemValue]);
+    // }
 
-    if (viewItemList.includes(itemValue)) {
+    if (itemChildValues.includes(itemValue)) {
       setViewItemList(viewItemList.filter((item) => item !== itemValue));
-    } else {
+    } else if (!itemChildValues.includes(itemValue)) {
       setViewItemList([...viewItemList, itemValue]);
     }
   };
 
   return { viewItemList, onClickItem };
 };
+
+// 클릭한 것이 상위 값인지 확인
+// 1. viewItemList에 해당 값의 child value가 전부 있는지 확인
+// 1-2. 전부 있으면 모두 제거 - root value, child value
+// 1-3. 전부 있지 않으면 모두 추가 - root value, child value
+// 클릭한 것이 하위 값인지 확인
+// 1. viewItemList에 해당 값 있는지 확인
+// 1-2. 있으면 제거 - child value
+// 1-3. 없으면 추가
