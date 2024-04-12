5adefb66299a20765ed49834a10e3601eaa26729
poeynus <poeynus@gmail.com>
2024-04-12 16:13:33 +0900

    링크 수정

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -14,18 +14,44 @@ const Map = () => {
   return (
     <div style={{ width: '100%', height: '100%', display: 'flex' }}>
       <MapSelector onClickMap={onClickMap} />
-      <Link to={'/boss'}>item/bag</Link>
-      <Link to={'/quest'}>item/bag</Link>
-      <Link to={'/ballistics'}>item/bag</Link>
-      <Link to={'/hideout'}>item/bag</Link>
-      <Link to={'/item/ammo'}>item/bag</Link>
-      <Link to={'/item/vest'}>item/bag</Link>
-      <Link to={'/item/rigs'}>item/bag</Link>
-      <Link to={'/item/container'}>item/bag</Link>
-      <Link to={'/item/headset'}>item/bag</Link>
-      <Link to={'/item/head-wear'}>item/bag</Link>
-      <Link to={'/item/medical'}>item/bag</Link>
-      <Link to={'/item/weapon'}>item/bag</Link>
+      <ul>
+        <li>
+          <Link to={'/boss'}>boss</Link>
+        </li>
+        <li>
+          <Link to={'/quest'}>quest</Link>
+        </li>
+        <li>
+          <Link to={'/ballistics'}>ballistics</Link>
+        </li>
+        <li>
+          <Link to={'/hideout'}>hideout</Link>
+        </li>
+        <li>
+          <Link to={'/item/ammo'}>ammo</Link>
+        </li>
+        <li>
+          <Link to={'/item/vest'}>vest</Link>
+        </li>
+        <li>
+          <Link to={'/item/rigs'}>rigs</Link>
+        </li>
+        <li>
+          <Link to={'/item/container'}>container</Link>
+        </li>
+        <li>
+          <Link to={'/item/headset'}>headset</Link>
+        </li>
+        <li>
+          <Link to={'/item/head-wear'}>head-wear</Link>
+        </li>
+        <li>
+          <Link to={'/item/medical'}>medical</Link>
+        </li>
+        <li>
+          <Link to={'/item/weapon'}>weapon</Link>
+        </li>
+      </ul>
       <MapView key={map.NAME} mapInfo={map} />
     </div>
   );
