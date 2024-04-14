9243313db53d9c2c2b8ae4c69a34f48f93cc5b55
poeynus <poeynus@gmail.com>
2024-04-14 12:37:15 +0900

    헤더 디자인 개발 완료

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PageRouter from 'src/routes/router';
+import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {
-  ChakraProvider,
-  CSSReset,
-  extendTheme,
-  Box,
-  Flex,
-  Heading,
-  Button,
-  VStack,
-  Text,
-} from '@chakra-ui/react';
+import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   components: {
@@ -34,109 +25,10 @@ const theme = extendTheme({
 });
 
 function App() {
-  const [selectedMenu, setSelectedMenu] = useState(null); // 선택된 메뉴를 추적하는 상태
-
-  // 메뉴를 변경하는 함수
-  const changeMenu = (menuName) => {
-    setSelectedMenu(menuName);
-  };
-
-  // 메뉴 목록 배열
-  const menuList = ['지도', '퀘스트', '아이템', '정보'];
-
-  // 하위 목록 배열
-  const subMenuList = {
-    지도: [
-      '세관',
-      '등대',
-      '산림',
-      '해안선',
-      '팩토리',
-      '리저브',
-      '연구소',
-      '인터체인지',
-      '그라운드 제로',
-      '타르코프 시내',
-    ],
-    퀘스트: [
-      '프라퍼',
-      '테라피스트',
-      '펜스',
-      '스키어',
-      '피스키퍼',
-      '메카닉',
-      '레그맨',
-      '예거',
-      '등대지기',
-    ],
-    아이템: [
-      '무기',
-      '총알',
-      '방탄모',
-      '의료품',
-      '컨테이너',
-      '전술 조끼',
-      '방탄 조끼',
-      '키',
-      '헤드셋',
-      '가방',
-    ],
-    정보: ['하이드 아웃', '보스'],
-  };
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
-      <Flex flexDirection="column">
-        <Box
-          position="fixed" /* 탑 네비게이션을 화면 상단에 고정시킵니다. */
-          top="0"
-          left="0"
-          width="100%"
-          zIndex="1000"
-          bg="#000000" // 검은색 배경 설정
-          boxShadow="0 2px 4px rgba(0, 0, 0, 0.1)"
-          backdropFilter="blur(10px)"
-        >
-          <Flex justifyContent="center" alignItems="center" height="50px">
-            <Heading fontSize="28px" cursor="pointer" color="white">
-              TKL
-            </Heading>
-            <Flex justifyContent="center" py="2">
-              {menuList.map((menuName, index) => (
-                <Button
-                  key={index}
-                  onMouseEnter={() => changeMenu(menuName)}
-                  variant={selectedMenu === menuName ? 'solid' : 'outline'}
-                  fontWeight="bold" // 텍스트를 굵게 설정합니다.
-                  borderWidth="2px"
-                  colorScheme="whiteAlpha" // 텍스트 및 테두리 색상을 지정합니다.
-                  m="2"
-                >
-                  {menuName}
-                  {selectedMenu === menuName && (
-                    <VStack
-                      spacing={2}
-                      align="stretch"
-                      p={2}
-                      position="absolute"
-                      top="50px"
-                      onMouseEnter={() => setSelectedMenu(menuName)}
-                      onMouseLeave={() => setSelectedMenu(null)}
-                      bg="rgba(128, 128, 128, 0.5)"
-                    >
-                      {subMenuList[selectedMenu].map((subMenuName, index) => (
-                        <Text key={index} fontSize="lg">
-                          {subMenuName}
-                        </Text>
-                      ))}
-                    </VStack>
-                  )}
-                </Button>
-              ))}
-            </Flex>
-          </Flex>
-        </Box>
-      </Flex>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<PageRouter.MAP />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,121 @@
+import {
+  Flex,
+  Box,
+  Heading,
+  VStack,
+  Button,
+  Text,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
 const Header = () => {
-  return <div>header</div>;
+  const [selectedMenu, setSelectedMenu] = useState(null); // 선택된 메뉴를 추적하는 상태
+
+  // 메뉴를 변경하는 함수
+  const changeMenu = (menuName) => {
+    setSelectedMenu(menuName);
+  };
+
+  // 메뉴 목록 배열
+  const menuList = ['지도', '퀘스트', '아이템', '정보'];
+
+  // 하위 목록 배열
+  const subMenuList = {
+    지도: [
+      '세관',
+      '등대',
+      '산림',
+      '해안선',
+      '팩토리',
+      '리저브',
+      '연구소',
+      '인터체인지',
+      '그라운드 제로',
+      '타르코프 시내',
+    ],
+    퀘스트: [
+      '프라퍼',
+      '테라피스트',
+      '펜스',
+      '스키어',
+      '피스키퍼',
+      '메카닉',
+      '레그맨',
+      '예거',
+      '등대지기',
+    ],
+    아이템: [
+      '무기',
+      '총알',
+      '방탄모',
+      '의료품',
+      '컨테이너',
+      '전술 조끼',
+      '방탄 조끼',
+      '키',
+      '헤드셋',
+      '가방',
+    ],
+    정보: ['하이드 아웃', '보스'],
+  };
+  return (
+    <Grid
+      templateColumns="repeat(3, 1fr)"
+      gap={6}
+      position={'fixed'}
+      width={'100%'}
+      zIndex={10}
+      bg={'tomato'}
+    >
+      <GridItem colSpan={1} h="14" />
+      <GridItem colSpan={1} h="14" textAlign={'center'}>
+        <Heading
+          as={'h1'}
+          size={'2xl'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          color={'white'}
+        >
+          TKL
+        </Heading>
+      </GridItem>
+      <GridItem colStart={3} colEnd={6} h="14" textAlign={'center'}>
+        {menuList.map((menuName, index) => (
+          <Button
+            key={index}
+            onMouseEnter={() => changeMenu(menuName)}
+            variant={selectedMenu === menuName ? 'solid' : 'outline'}
+            fontWeight="bold"
+            borderWidth="2px"
+            colorScheme="whiteAlpha"
+            m="2"
+          >
+            {menuName}
+            {selectedMenu === menuName && (
+              <VStack
+                spacing={2}
+                align="stretch"
+                p={2}
+                position="absolute"
+                top="50px"
+                onMouseEnter={() => setSelectedMenu(menuName)}
+                onMouseLeave={() => setSelectedMenu(null)}
+                bg="rgba(128, 128, 128, 0.5)"
+              >
+                {subMenuList[selectedMenu].map((subMenuName, index) => (
+                  <Text key={index} fontSize="lg">
+                    {subMenuName}
+                  </Text>
+                ))}
+              </VStack>
+            )}
+          </Button>
+        ))}
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default Header;
