20c6f109fa824d8bfee7a9d4a0ed1cefd333c8ef
poeynus <poeynus@gmail.com>
2024-04-28 19:15:21 +0900

    지도 wasd 구현 - 버그 있음

--- src/components/Map/View/ThreeView.js
+++ src/components/Map/View/ThreeView.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { Canvas, useThree, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas, useThree } from '@react-three/fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { ALL_ITEM } from 'src/utils/itemConstants';
 import { MAP_COLOR, ALL_COLOR } from 'src/utils/colorConstants';
 import MapViewSkeleton from 'src/components/Map/View/MapViewSkeleton';
@@ -9,17 +9,85 @@ import PropTypes from 'prop-types';
 
 const ThreeView = ({ map, viewItemList }) => {
   const collada = hooks.useLoadMap(map.map_three_path, MAP_COLOR.MAP_BLACK);
+  const ExtendedOrbitControls = () => {
+    const movementSpeed = 0.5;
+    const { camera, gl } = useThree();
+    const keys = useRef({});
+
+    useEffect(() => {
+      const controls = new OrbitControls(camera, gl.domElement);
+      controls.enableKeys = false; // 키보드 조작 비활성화
+
+      const handleKeyDown = (event) => {
+        keys.current[event.code] = true;
+      };
+
+      const handleKeyUp = (event) => {
+        keys.current[event.code] = false;
+      };
+
+      document.addEventListener('keydown', handleKeyDown);
+      document.addEventListener('keyup', handleKeyUp);
+
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+        document.removeEventListener('keyup', handleKeyUp);
+      };
+    }, [camera, gl]);
+
+    useEffect(() => {
+      const updateCameraPosition = () => {
+        const { x, y, z } = camera.position;
+        const moveDistance = movementSpeed;
+        console.log(keys.current['KeyW']);
+        if (keys.current['KeyW']) {
+          camera.position.set(x, y, z - moveDistance);
+        }
+        if (keys.current['KeyS']) {
+          camera.position.set(x, y, z + moveDistance);
+        }
+        if (keys.current['KeyA']) {
+          camera.position.set(x - moveDistance, y, z);
+        }
+        if (keys.current['KeyD']) {
+          camera.position.set(x + moveDistance, y, z);
+        }
+      };
+
+      const animate = () => {
+        if (
+          keys.current['KeyW'] ||
+          keys.current['KeyS'] ||
+          keys.current['KeyA'] ||
+          keys.current['KeyD']
+        ) {
+          updateCameraPosition();
+        }
+
+        requestAnimationFrame(animate);
+      };
+
+      animate();
+
+      return () => {
+        cancelAnimationFrame(animate);
+      };
+    }, [camera, movementSpeed]);
+
+    return null;
+  };
   if (!collada) return null;
 
   return (
     <Canvas
-      camera={{ position: [0, 10, 10] }}
+      camera={{ position: [0, 30, 30] }}
       style={{
         backgroundColor: hooks.useHexFromDecimal(ALL_COLOR.BLACK_90),
         height: '100vh',
       }}
     >
       <axesHelper scale={10} />
+      <ExtendedOrbitControls />
       <ambientLight intensity={2} />
       <pointLight position={[0, 0, 0]} intensity={2} />
       <group
@@ -47,7 +115,6 @@ const ThreeView = ({ map, viewItemList }) => {
             ),
         )}
       </group>
-      <OrbitControls />
     </Canvas>
   );
 };
