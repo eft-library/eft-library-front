308d8b6b39e1fc252b78b9d83c0b539d0a9149b9
poeynus <poeynus@gmail.com>
2024-06-08 21:13:17 +0900

    로고 변경

--- ssr-front/src/app/not-found.tsx
+++ ssr-front/src/app/not-found.tsx
@@ -61,43 +61,10 @@ export default function NotFound() {
             </Box>
           </GridItem>
           <Logo
-            width={size.width ? size.width / 2.5 : 400}
-            height={size.height ? size.height / 2.5 : 300}
+            width={size.width ? size.width / 3 : 400}
+            height={size.height ? size.height / 3 : 300}
           />
         </SimpleGrid>
-        {/* <Box
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"space-evenly"}
-        >
-          <Box display={"flex"} flexDirection={"column"}>
-            <Text
-              color={ALL_COLOR.WHITE}
-              mb={6}
-              style={{ fontSize: size.width ? size.width / 30 : "inherit" }}
-            >
-              404 ERROR
-            </Text>
-            <Text
-              style={{ fontSize: size.width ? size.width / 80 : "inherit" }}
-              color={ALL_COLOR.WHITE}
-              mb={2}
-            >
-              Page Not Found
-            </Text>
-            <Text
-              size={"sm"}
-              color={ALL_COLOR.WHITE}
-              style={{ fontSize: size.width ? size.width / 80 : "inherit" }}
-            >
-              죄송합니다. 페이지를 찾을 수 없습니다.
-            </Text>
-          </Box>
-          <Logo
-            width={size.width ? size.width / 2 : 400}
-            height={size.height ? size.height / 2 : 300}
-          />
-        </Box> */}
       </Flex>
     </Box>
   );
