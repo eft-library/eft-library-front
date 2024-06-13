e3d371c019ee5d05b6165f565cd5fd215ebd1d2d
poeynus <poeynus@gmail.com>
2024-06-13 21:00:20 +0900

    naver login 추가

--- ssr-front/src/app/api/auth/[...nextauth]/route.ts
+++ ssr-front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import Naver from "next-auth/providers/naver";
 
 const handler = NextAuth({
   providers: [
     Google({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_ID || "",
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET || "",
+    }),
+    Naver({
+      clientId: process.env.NEXT_PUBLIC_NAVER_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_NAVER_SECRET || "",
     }),
   ],
 });
