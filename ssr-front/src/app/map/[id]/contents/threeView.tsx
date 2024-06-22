031de9bd01b2a862b75a3407c966dd748c2f263a
poeynus <poeynus@gmail.com>
2024-06-22 19:58:31 +0900

    3d 테두리 구현 완료 ....!

--- ssr-front/src/app/map/[id]/contents/threeView.tsx
+++ ssr-front/src/app/map/[id]/contents/threeView.tsx
@@ -6,21 +6,11 @@ import { Canvas } from "@react-three/fiber";
 import type { ThreeView } from "@/types/types";
 import ThreeSkeleton from "../skeleton/threeSkeleton";
 import useColorValue from "@/hooks/useColorValue";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, Edges } from "@react-three/drei";
 import { formatImage } from "@/lib/formatImage";
-// import { useEffect } from "react";
-// import * as THREE from "three";
-// import { useLoadMap } from "@/hooks/useMap";
 
 export default function ThreeView({ map, viewItemList }: ThreeView) {
   const { threBack } = useColorValue();
-
-  // 성능 개선 이전 사용하던 코드 - dae 파일을 불러온 뒤 traverse로 각 line을 색칠했음
-  // 현재는 export edge 옵션을 추가해서 gltf로 추출하고, three 라이브러리 lineBasicMaterial 색상을 수정했음
-  // const collada = useLoadMap(map.three_image, true);
-
-  // if (!collada) return <ThreeSkeleton />;
-
   const { nodes, materials } = useGLTF(formatImage(map.three_image)) as any;
 
   if (!nodes || !materials) return <ThreeSkeleton />;
@@ -33,7 +23,12 @@ export default function ThreeView({ map, viewItemList }: ThreeView) {
         height: "100vh",
       }}
     >
-      <MapControls />
+      <MapControls
+        zoomSpeed={2.0}
+        enableDamping
+        dampingFactor={0.1} // 감속 계수 설정 (0에서 1 사이의 값)
+        enableZoom={true}
+      />
       <ambientLight intensity={2} />
       <pointLight position={[0, 0, 0]} intensity={2} />
       <group
@@ -42,19 +37,11 @@ export default function ThreeView({ map, viewItemList }: ThreeView) {
         }}
       >
         <mesh
-          geometry={nodes.mesh_0.geometry}
-          material={materials.PaletteMaterial002}
-        />
-        <mesh
-          geometry={nodes.mesh_0_1.geometry}
-          material={materials["신형 아스팔트"]}
-        />
-        <mesh
           geometry={nodes.mesh_0_2.geometry}
           material={materials.PaletteMaterial001}
-        />
-        {/* <primitive object={collada.colladaData.scene} position={[0, 0, 0]} /> */}
-
+        >
+          <Edges visible={true} scale={1} color="black" />
+        </mesh>
         {map.three_item_path.map(
           (item) =>
             viewItemList.includes(item.childValue) && (
