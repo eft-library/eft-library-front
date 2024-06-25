d4ba2b828b3be078e6e85e99fba046447ed33e68
poeynus <poeynus@gmail.com>
2024-06-25 11:31:24 +0900

    일단 next auth 타입 선언 추가

--- ssr-front/src/components/login/login.tsx
+++ ssr-front/src/components/login/login.tsx
@@ -2,8 +2,10 @@ import { Button } from "@chakra-ui/react";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Login() {
-  const { data: session } = useSession();
+  const { data: session, update } = useSession();
+  const { accessToken } = session;
 
+  // () => update() - 세션 업데이트 가능
   if (session) {
     return <Button onClick={() => signOut()}>{session.user.name}</Button>;
   }
---
+++ ssr-front/src/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import { DefaultSession } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
+import { JWT } from "next-auth/jwt";
+
+// 세션에 토큰 정보 추가
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken: string;
+  }
+}
+
+// jwt에 토큰 정보 추가
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken: string;
+  }
+}
