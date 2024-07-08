a976f29830cc6a91d53996f1e30ced2c4da152b1
poeynus <poeynus@gmail.com>
2024-07-08 14:39:12 +0900

    3d 화면 로딩 수정정

--- ssr-front/src/app/map/[id]/contents/three/loader.tsx
+++ ssr-front/src/app/map/[id]/contents/three/loader.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { useProgress, Html } from "@react-three/drei";
+import { Box, Image, Text } from "@chakra-ui/react";
 
 export default function Loader() {
   const { progress } = useProgress();
-  return <Html center>{progress.toFixed(2)}% loaded</Html>;
+  return (
+    <Html center>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
+        <Box
+          width="600px"
+          height="auto"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Image src="/loading.gif" alt="loading" />
+        </Box>
+        <Text fontWeight={600}>{progress.toFixed(2)}% 로딩중...</Text>
+      </Box>
+    </Html>
+  );
 }
