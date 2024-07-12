e2c1f34f4f6f48685308d8a708b9cc8813761788
poeynus <poeynus@gmail.com>
2024-07-12 19:24:04 +0900

    sitemap 작성

--- ssr-front/src/app/user/quest/page.tsx
+++ ssr-front/src/app/user/quest/page.tsx
@@ -4,6 +4,7 @@ import PageParent from "@/components/pageParent/pageParent";
 import SubHeader from "@/components/subHeader/subHeader";
 import UserQuestDetail from "./contents/userQuestDetail";
 import type { UserQuest } from "@/types/types";
+import "@/assets/quest.css";
 
 export default function UserQuest() {
   return (
---
+++ ssr-front/src/sitemap.ts
@@ -0,0 +1,114 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://eftlibrary.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://eftlibrary.com/armor-vest",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://eftlibrary.com/backpack",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/boss",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/container",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/face-cover",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/headset",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/head-wear",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/hideout",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/key",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/loot",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/map-of-tarkov",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/medical",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/provisions",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/quest",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/rig",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/weapon",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://eftlibrary.com/quest/detail",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+  ];
+}
