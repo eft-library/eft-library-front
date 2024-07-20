ad7d28339cc68b11c7dea581550355058917ff27
poeynus <poeynus@gmail.com>
2024-07-20 09:12:39 +0900

    퀘스트 상세 버그 수정

--- ssr-front/src/app/quest/detail/[id]/contents/questContents.tsx
+++ ssr-front/src/app/quest/detail/[id]/contents/questContents.tsx
@@ -61,113 +61,112 @@ export default function QuestContents({ quest }: QuestContents) {
           ))}
         </Box>
       </DividerContents>
-      {(quest.sub && quest.sub.length > 0) ||
-        (quest.guide && (
-          <DividerContents headText="가이드">
-            {quest.sub && quest.sub.length > 0 && (
-              <>
-                <Text fontWeight={800} mb={4}>
-                  관련 퀘스트 아이템
-                </Text>
-                <Box
-                  display={"flex"}
-                  flexDirection={"column"}
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                >
-                  <GridTitle
-                    columnDesign={[2, null, 6]}
-                    column={column.value_kr}
-                    isShadow
-                    shadowColor={ALL_COLOR.YELLOW_SHADOW}
-                    isQuest
-                    titleWidth="100%"
-                  />
-                  {quest.sub.map((item) => (
-                    <GridContents
-                      contentsWidth="100%"
-                      columnDesign={[2, null, 6]}
-                      key={item.item_id}
-                      id={item.item_id}
-                    >
-                      <Box
-                        display={"flex"}
-                        alignItems={"center"}
-                        justifyContent={"center"}
-                      >
-                        <ImageZoom
-                          originalImg={item.item_image}
-                          thumbnail={item.item_image}
-                        />
-                      </Box>
-                      <Link
-                        href={`${item.item_link}${item.item_id}`}
-                        scroll={false}
-                      >
-                        <GridCenterText isHover>
-                          {item.item_name_kr}
-                        </GridCenterText>
-                      </Link>
-                      <GridCenterText>{item.count}</GridCenterText>
-                      <Box
-                        w={"100%"}
-                        h={"100%"}
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                        flexDirection={"column"}
-                      >
-                        {item.type !== "KEY" && (
-                          <Text
-                            color={
-                              item.in_raid ? ALL_COLOR.RED : ALL_COLOR.YELLOW
-                            }
-                            _hover={{ color: ALL_COLOR.BEIGE }}
-                            textAlign="center"
-                            fontWeight={600}
-                          >
-                            {item.in_raid ? "Y" : "N"}
-                          </Text>
-                        )}
-                      </Box>
-                      <GridItem
-                        colSpan={2}
-                        display={"flex"}
-                        alignItems={"center"}
-                        justifyContent={"center"}
-                      >
-                        {item.desc_text &&
-                          item.desc_text.map((desc, index) => (
-                            <Box
-                              key={index}
-                              display={"flex"}
-                              alignItems={"center"}
-                              justifyContent={"center"}
-                            >
-                              <Text>{desc}</Text>
-                            </Box>
-                          ))}
-                      </GridItem>
-                    </GridContents>
-                  ))}
-                </Box>
-              </>
-            )}
-            {quest.guide && (
-              <Box>
-                <Text
-                  color={ALL_COLOR.WHITE}
-                  mt={1}
-                  fontWeight={700}
-                  fontSize="lg"
-                  dangerouslySetInnerHTML={{
-                    __html: `${quest.guide}`,
-                  }}
+      {((quest.sub && quest.sub.length > 0) || quest.guide) && (
+        <DividerContents headText="가이드">
+          {quest.sub && quest.sub.length > 0 && (
+            <>
+              <Text fontWeight={800} mb={4}>
+                관련 퀘스트 아이템
+              </Text>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <GridTitle
+                  columnDesign={[2, null, 6]}
+                  column={column.value_kr}
+                  isShadow
+                  shadowColor={ALL_COLOR.YELLOW_SHADOW}
+                  isQuest
+                  titleWidth="100%"
                 />
+                {quest.sub.map((item) => (
+                  <GridContents
+                    contentsWidth="100%"
+                    columnDesign={[2, null, 6]}
+                    key={item.item_id}
+                    id={item.item_id}
+                  >
+                    <Box
+                      display={"flex"}
+                      alignItems={"center"}
+                      justifyContent={"center"}
+                    >
+                      <ImageZoom
+                        originalImg={item.item_image}
+                        thumbnail={item.item_image}
+                      />
+                    </Box>
+                    <Link
+                      href={`${item.item_link}${item.item_id}`}
+                      scroll={false}
+                    >
+                      <GridCenterText isHover>
+                        {item.item_name_kr}
+                      </GridCenterText>
+                    </Link>
+                    <GridCenterText>{item.count}</GridCenterText>
+                    <Box
+                      w={"100%"}
+                      h={"100%"}
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center"
+                      flexDirection={"column"}
+                    >
+                      {item.type !== "KEY" && (
+                        <Text
+                          color={
+                            item.in_raid ? ALL_COLOR.RED : ALL_COLOR.YELLOW
+                          }
+                          _hover={{ color: ALL_COLOR.BEIGE }}
+                          textAlign="center"
+                          fontWeight={600}
+                        >
+                          {item.in_raid ? "Y" : "N"}
+                        </Text>
+                      )}
+                    </Box>
+                    <GridItem
+                      colSpan={2}
+                      display={"flex"}
+                      alignItems={"center"}
+                      justifyContent={"center"}
+                    >
+                      {item.desc_text &&
+                        item.desc_text.map((desc, index) => (
+                          <Box
+                            key={index}
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent={"center"}
+                          >
+                            <Text>{desc}</Text>
+                          </Box>
+                        ))}
+                    </GridItem>
+                  </GridContents>
+                ))}
               </Box>
-            )}
-          </DividerContents>
-        ))}
+            </>
+          )}
+          {quest.guide && (
+            <Box>
+              <Text
+                color={ALL_COLOR.WHITE}
+                mt={1}
+                fontWeight={700}
+                fontSize="lg"
+                dangerouslySetInnerHTML={{
+                  __html: `${quest.guide}`,
+                }}
+              />
+            </Box>
+          )}
+        </DividerContents>
+      )}
     </Box>
   );
 }
