a2fdf75caca849dba0cf57a745842aec080dffba
poeynus <poeynus@gmail.com>
2024-08-13 14:54:28 +0900

    게시판 metadata generate

--- src/app/board/tip/detail/[id]/page.tsx
+++ src/app/board/tip/detail/[id]/page.tsx
@@ -1,4 +1,35 @@
 import DetailMain from "@/components/boardDetail/detailMain";
+import { Metadata, ResolvingMetadata } from "next";
+import { formatImage } from "@/lib/formatImage";
+import type { MetaProps } from "@/types/types";
+import API_ENDPOINTS from "@/config/endPoints";
+
+export async function generateMetadata(
+  { params, searchParams }: MetaProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // params에서 id 추출
+  const id = params.id;
+
+  // fetch data
+  const product = await fetch(
+    `${API_ENDPOINTS.GET_BOARD_BY_TYPE}/tip/detail?board_id=${id}`
+  ).then((res) => res.json());
+
+  const res = product.data;
+
+  return {
+    title: `${res.title}`,
+    description: `${res.title}`,
+    openGraph: {
+      images: res.image ? formatImage(res.image) : "/og.png",
+      title: `EFT Library ${res.title}`,
+      description: `EFT Library ${res.title}`,
+      url: `https://eftlibrary.com/board/tip/detail/${id}`,
+      siteName: "Escape From Tarkov Library",
+    },
+  };
+}
 
 export default function TipDetail() {
   return <DetailMain siteParam="tip" />;
