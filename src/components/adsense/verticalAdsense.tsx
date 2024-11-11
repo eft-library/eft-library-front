fd31b7685c32470819c413a85413ea09540249f5
poeynus <poeynus@gmail.com>
2024-11-11 20:30:58 +0900

    광고 수정 ㅠㅠ

--- src/components/adsense/verticalAdsense.tsx
+++ src/components/adsense/verticalAdsense.tsx
@@ -6,15 +6,13 @@ export default function VerticalAdsense() {
   }, []);
 
   return (
-      <div className="googleAd-container">
-        <ins
-            className="adsbygoogle"
-            style={{display: 'block', width: '100%', height: '100px', marginBottom: '20px'}}
-            data-ad-client={process.env.NEXT_PUBLIC_ADSENSE}
-            data-ad-slot={process.env.NEXT_PUBLIC_ADSENSE_VERTICAL_SLOT}
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-        ></ins>
-      </div>
+      <ins
+          className="adsbygoogle"
+          style={{display: 'block', width: '100%', height: '100px', marginBottom: '20px'}}
+          data-ad-client={process.env.NEXT_PUBLIC_ADSENSE}
+          data-ad-slot={process.env.NEXT_PUBLIC_ADSENSE_VERTICAL_SLOT}
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+      ></ins>
   );
 };
