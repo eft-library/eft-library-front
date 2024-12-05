94d9ac25c5567bc08374514a02734bd25d658087
poeynus <poeynus@gmail.com>
2024-12-05 14:43:21 +0900

    광고 부분 수정정

--- src/app/backpack/contents/backpackDetail.tsx
+++ src/app/backpack/contents/backpackDetail.tsx
@@ -14,7 +14,6 @@ import ImageZoom from "@/components/imageZoom/imageZoom";
 import WeaponSkeleton from "@/app/weapon/contents/skeleton/weaponSkeleton";
 import { useScrollMove } from "@/hooks/useScrollMove";
 import { ALL_COLOR } from "@/util/consts/colorConsts";
-import VerticalAdsense from "@/components/adsense/verticalAdsense";
 
 export default function BackpackDetail() {
   const param = useSearchParams();
@@ -34,27 +33,26 @@ export default function BackpackDetail() {
   if (!backpackList || !column) return <WeaponSkeleton />;
 
   return (
-      <>
-          <VerticalAdsense />
-          <GridTitle
-              columnDesign={[2, null, 5]}
-              column={column.value_kr}
-              isShadow
-              shadowColor={ALL_COLOR.YELLOW_SHADOW}
-          />
-          {backpackList.map((item) => (
-              <GridContents columnDesign={[2, null, 5]} key={item.id} id={item.id}>
-                  <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
-                      <ImageZoom originalImg={item.image} thumbnail={item.image}/>
-                  </Box>
-                  <GridCenterText>{item.name}</GridCenterText>
-                  <GridCenterText>{item.capacity} </GridCenterText>
-                  <GridCenterText>
-                      {item.grids[0].width} x {item.grids[0].height}
-                  </GridCenterText>
-                  <GridCenterText>{item.weight} kg</GridCenterText>
-              </GridContents>
-          ))}
-      </>
+    <>
+      <GridTitle
+        columnDesign={[2, null, 5]}
+        column={column.value_kr}
+        isShadow
+        shadowColor={ALL_COLOR.YELLOW_SHADOW}
+      />
+      {backpackList.map((item) => (
+        <GridContents columnDesign={[2, null, 5]} key={item.id} id={item.id}>
+          <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+            <ImageZoom originalImg={item.image} thumbnail={item.image} />
+          </Box>
+          <GridCenterText>{item.name}</GridCenterText>
+          <GridCenterText>{item.capacity} </GridCenterText>
+          <GridCenterText>
+            {item.grids[0].width} x {item.grids[0].height}
+          </GridCenterText>
+          <GridCenterText>{item.weight} kg</GridCenterText>
+        </GridContents>
+      ))}
+    </>
   );
 }
