98c18ab73f7a955b918e16ebe4367e93bb435898
poeynus <poeynus@gmail.com>
2025-02-05 15:00:08 +0900

    meta 수정

--- app/price/page.tsx
+++ app/price/page.tsx
@@ -3,19 +3,19 @@ import Price from "@/components/page/price/price";
 export const dynamic = "force-dynamic";
 
 export const metadata = {
-  title: "EFT Library 아이템 시세 - EFT Library",
-  description: "EFT Library 아이템 시세 페이지.",
+  title: "타르코프 아이템 시세 - EFT Library",
+  description: "타르코프 아이템 시세 페이지.",
   openGraph: {
     siteName: "EFT Library",
-    title: "EFT Library 아이템 시세 - EFT Library",
-    description: "EFT Library 아이템 시세 페이지.",
+    title: "타르코프 아이템 시세 - EFT Library",
+    description: "타르코프 아이템 시세 페이지.",
     images: "/og.png",
     url: "https://eftlibrary.com/price",
   },
   twitter: {
     siteName: "EFT Library",
-    title: "EFT Library 아이템 시세 - EFT Library",
-    description: "EFT Library  아이템 시세 페이지.",
+    title: "타르코프 아이템 시세 - EFT Library",
+    description: "타르코프 아이템 시세 페이지.",
     images: "/og.png",
     url: "https://eftlibrary.com/price",
   },
