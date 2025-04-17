b1f087f9b3b09ba7e006e3ffb53b7c6ed8743a90
poeynus <poeynus@gmail.com>
2025-04-17 09:35:20 +0900

    내 위치 찾기 일단 좌표까진 출력

--- components/page/mapOfTarkov/data/mapSlider.tsx
+++ components/page/mapOfTarkov/data/mapSlider.tsx
@@ -6,8 +6,26 @@ import "photoswipe/dist/photoswipe.css";
 import Image from "next/image";
 import TextSpan from "../../../custom/gridContents/textSpan";
 import type { MapSlider } from "./mapOfTarkovType";
+import { useState } from "react";
+import { MapContainer, ImageOverlay, useMapEvent } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MouseMoveEvent = ({ onMove }: { onMove: (latlng: any) => void }) => {
+  useMapEvent("mousemove", (e) => {
+    onMove(e.latlng); // 마우스 위치 좌표 업데이트
+  });
+  return null;
+};
 
 export default function MapSlider({ mapInfo, imageSelect }: MapSlider) {
+  const [mousePosition, setMousePosition] = useState<{
+    lat: number;
+    lng: number;
+  }>({ lat: 0, lng: 0 });
+  const imageSrc =
+    "https://image.eftlibrary.com/eftlibrary/tkl_map/completed_map/customs.webp";
+
   return (
     <div className="w-full flex flex-col gap-4">
       <div className="flex justify-between items-center">
@@ -16,7 +34,28 @@ export default function MapSlider({ mapInfo, imageSelect }: MapSlider) {
         </TextSpan>
       </div>
       <Separator className="bg-white" />
-      <Gallery>
+      <div>
+        <p>Latitude: {mousePosition.lat.toFixed(2)}</p>
+        <p>Longitude: {mousePosition.lng.toFixed(2)}</p>
+      </div>
+      <MapContainer
+        center={[387.5, 692.5]}
+        zoom={0}
+        minZoom={-1}
+        maxZoom={4}
+        crs={L.CRS.Simple}
+        style={{ width: "100%", height: "800px" }}
+      >
+        <MouseMoveEvent onMove={setMousePosition} />
+        <ImageOverlay
+          url={imageSrc}
+          bounds={[
+            [0, 0],
+            [778, 1385],
+          ]}
+        />
+      </MapContainer>
+      {/* <Gallery>
         {mapInfo.sub.map(
           (map) =>
             map.id === imageSelect && (
@@ -50,7 +89,7 @@ export default function MapSlider({ mapInfo, imageSelect }: MapSlider) {
               </Item>
             )
         )}
-      </Gallery>
+      </Gallery> */}
     </div>
   );
 }
