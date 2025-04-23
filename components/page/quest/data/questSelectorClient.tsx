b3fa750e7eb8a9f5e2f74b70851ea29bad875cca
poeynus <poeynus@gmail.com>
2025-04-24 08:01:55 +0900

    quest npc 배치 수정

--- components/page/quest/data/questSelectorClient.tsx
+++ components/page/quest/data/questSelectorClient.tsx
@@ -8,27 +8,31 @@ export default function QuestSelectorClient({ npcList }: QuestSelectorClient) {
   const { npcId, setNpcId } = useAppStore((state) => state);
 
   return (
-    <div className="grid grid-cols-6 gap-4 justify-items-center w-[70%]">
-      {npcList.map((npc) => (
-        <div className="flex flex-col items-center" key={npc.id}>
-          <div
-            className={`cursor-pointer w-[140px] h-[140px] rounded-lg ${
-              npcId === npc.id
-                ? "outline outline-4 outline-[color:Goldenrod]"
-                : "outline outline-2 outline-[color:white]"
-            }`}
-            style={{
-              backgroundImage: `url(${npc.image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-            onClick={() => setNpcId(npc.id)}
-            onMouseEnter={handleHover}
-            onMouseLeave={handleHoverExit}
-          />
-          <p className="text-white text-center mt-2 font-bold">{npc.name_kr}</p>
-        </div>
-      ))}
+    <div className="w-full flex justify-center items-center">
+      <div className="grid grid-cols-6 gap-4 w-[70%]">
+        {npcList.map((npc) => (
+          <div className="flex flex-col items-center" key={npc.id}>
+            <div
+              className={`cursor-pointer w-[140px] h-[140px] rounded-lg ${
+                npcId === npc.id
+                  ? "outline outline-4 outline-[color:Goldenrod]"
+                  : "outline outline-2 outline-[color:white]"
+              }`}
+              style={{
+                backgroundImage: `url(${npc.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              onClick={() => setNpcId(npc.id)}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHoverExit}
+            />
+            <p className="text-white text-center mt-2 font-bold">
+              {npc.name_kr}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
