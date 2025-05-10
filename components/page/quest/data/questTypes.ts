00416a73f546263f17d925b8934e798f3a099ca8
poeynus <poeynus@gmail.com>
2025-05-11 08:49:21 +0900

    퀘스트 보상 관련 정보 타입 수정

--- components/page/quest/data/questTypes.ts
+++ components/page/quest/data/questTypes.ts
@@ -76,6 +76,42 @@ interface LocaleName {
 
 interface FinishRewards {
   items: RewardItem[];
+  offerUnlock: OfferUnlock[];
+  traderStanding: TraderStanding[];
+  craftUnlock: CraftUnlock[];
+}
+
+interface CraftUnlock {
+  level: number;
+  station: StationDetail;
+  rewardItems: BarterItem[];
+}
+
+interface StationDetail {
+  id: string;
+  name_en: string;
+  name_ja: string;
+  name_ko: string;
+}
+
+interface TraderStanding {
+  trader: TraderDetail;
+  standing: number;
+}
+
+interface OfferUnlock {
+  item: RewardItemDetails;
+  level: number;
+  trader: TraderDetail;
+}
+
+interface TraderDetail {
+  id: string;
+  name_en: string;
+  name_ja: string;
+  name_ko: string;
+  imageLink: string;
+  normalizedName: string;
 }
 
 interface RewardItem {
