8d0a2b3a09929bf7bf3e8f6b85ed8f25e4cd155f
poeynus <poeynus@gmail.com>
2025-05-12 07:54:35 +0900

    store 정리

--- store/store.ts
+++ store/store.ts
@@ -1,4 +1,3 @@
-// store.ts
 import { createStore } from "zustand";
 import { persist } from "zustand/middleware";
 import type { ItemType } from "@/lib/func/itemFilterUtils";
@@ -6,17 +5,11 @@ import type { ItemType } from "@/lib/func/itemFilterUtils";
 export type AppStateType = {
   npcId: string;
   newItemFilter: ItemType[];
-  eventNum: number;
-  noticeNum: number;
-  patchNotesNum: number;
 };
 
 export type AppActionsType = {
   setNpcId: (value: string) => void;
   setItemFilter: (value: ItemType[]) => void;
-  setEventNum: (value: number) => void;
-  setNoticeNum: (value: number) => void;
-  setPatchNotesNum: (value: number) => void;
 };
 
 export type AppStoreType = AppStateType & AppActionsType;
@@ -24,9 +17,6 @@ export type AppStoreType = AppStateType & AppActionsType;
 export const defaultInitState: AppStateType = {
   npcId: "54cb50c76803fa8b248b4571",
   newItemFilter: [],
-  noticeNum: 1,
-  eventNum: 1,
-  patchNotesNum: 1,
 };
 
 export const createAppStore = (initState: AppStateType = defaultInitState) => {
@@ -37,9 +27,6 @@ export const createAppStore = (initState: AppStateType = defaultInitState) => {
           ...initState,
           setNpcId: (value: string) => set({ npcId: value }),
           setItemFilter: (value: ItemType[]) => set({ newItemFilter: value }),
-          setEventNum: (value: number) => set({ eventNum: value }),
-          setPatchNotesNum: (value: number) => set({ patchNotesNum: value }),
-          setNoticeNum: (value: number) => set({ noticeNum: value }),
         } satisfies AppStoreType),
       {
         name: "app-store",
