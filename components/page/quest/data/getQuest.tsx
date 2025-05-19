5a93c00efc6b99fa49d658146da7137849cd700c
poeynus <poeynus@gmail.com>
2025-05-20 08:33:35 +0900

    quest api 성능 수정

--- components/page/quest/data/getQuest.tsx
+++ components/page/quest/data/getQuest.tsx
@@ -1,16 +1,45 @@
-"use server";
+"use client";
 
 import { requestData } from "@/lib/config/api";
 import { API_ENDPOINTS } from "@/lib/config/endpoint";
 import QuestClient from "./questClient";
+import { Quest } from "./questTypes";
+import { useEffect, useState } from "react";
+import Loading from "@/components/custom/loading/loading";
+import { useAppStore } from "@/store/provider";
 
-export default async function GetQuest() {
-  const data = await requestData(API_ENDPOINTS.GET_ALL_QUEST);
+export default function GetQuest() {
+  const { npcId } = useAppStore((state) => state);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [questList, setQuestList] = useState<Quest[]>();
 
-  if (!data || data.status !== 200) {
-    console.error("Failed to fetch quest data:", data?.msg || "Unknown error");
-    return null;
-  }
+  useEffect(() => {
+    const getQuestById = async () => {
+      setIsLoading(true);
+      const data = await requestData(
+        `${API_ENDPOINTS.GET_QUEST_BY_NPC}/${npcId}`
+      );
 
-  return <QuestClient questList={data.data} />;
+      if (!data || data.status !== 200) {
+        console.error(
+          "Failed to fetch quest data:",
+          data?.msg || "Unknown error"
+        );
+        setIsLoading(false);
+        return null;
+      }
+
+      setQuestList(data.data);
+      setIsLoading(false);
+    };
+
+    getQuestById();
+  }, [npcId]);
+
+  return (
+    <>
+      {questList && <QuestClient questList={questList} />}
+      {isLoading && <Loading />}
+    </>
+  );
 }
--- components/page/quest/data/questClient.tsx
+++ components/page/quest/data/questClient.tsx
@@ -9,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useAppStore } from "@/store/provider";
 import Link from "next/link";
 import type { QuestClient } from "./questTypes";
 import { Skull, SquareCheckBig, SquareX } from "lucide-react";
@@ -25,7 +24,6 @@ import { questI18N } from "@/lib/consts/i18nConsts";
 export default function QuestClient({ questList }: QuestClient) {
   const locale = useLocale();
   const localeKey = getLocaleKey(locale);
-  const { npcId } = useAppStore((state) => state);
 
   return (
     <div className="w-full">
@@ -47,110 +45,100 @@ export default function QuestClient({ questList }: QuestClient) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {questList.map(
-            (quest) =>
-              npcId === null ||
-              (npcId === quest.npc_id && (
-                <TableRow key={quest.id} className="hover:bg-NeutralGray">
-                  <TableCell>
-                    <Link href={`/quest/detail/${quest.url_mapping}`}>
-                      <span className="text-base font-bold text-Orange hover:text-Beige flex text-center flex items-center justify-center">
-                        {quest.name[localeKey]}
-                      </span>
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    {quest.objectives &&
-                      quest.objectives.map((obj, oIndex) => (
-                        <div
-                          className="font-bold text-base p-[1px]"
-                          key={`${oIndex}-objectives-${quest.id}`}
-                        >
-                          * {obj[getDescriptionLocaleKey(locale)]}
-                          {obj.type === "shoot" && (
-                            <span className="ml-2 whitespace-nowrap">
-                              [
-                              <Skull
-                                className="inline-block w-4 h-4"
-                                color={ALL_COLOR.Red}
-                                strokeWidth={3}
-                              />
-                              x&nbsp;{obj.count}]
-                            </span>
-                          )}
-                        </div>
-                      ))}
-                  </TableCell>
-                  <TableCell>
-                    {quest.finish_rewards.items.map((rewards, rIndex) => (
-                      <div
-                        key={`${rIndex}-rewards-${quest.id}`}
-                        className="font-bold text-base p-[1px]"
-                      >
-                        * {rewards.item[getOtherLocalizedKey(locale)]} x&nbsp;
-                        {rewards.quantity}
-                      </div>
-                    ))}
-                    {quest.finish_rewards.offerUnlock.map((offer, rIndex) => (
-                      <div
-                        key={`${rIndex}-offerUnlock-${quest.id}`}
-                        className="font-bold text-base p-[1px]"
-                      >
-                        * {offer.trader[getOtherLocalizedKey(locale)]}&nbsp;
-                        {offer.item[getOtherLocalizedKey(locale)]}&nbsp;
-                        {questI18N.purchaseUnlock[localeKey]}
-                      </div>
-                    ))}
-                    {quest.finish_rewards.traderStanding.map(
-                      (standing, rIndex) => (
-                        <div
-                          key={`${rIndex}-traderStanding-${quest.id}`}
-                          className="font-bold text-base p-[1px]"
-                        >
-                          * {standing.trader[getOtherLocalizedKey(locale)]}
-                          &nbsp;{questI18N.standing[localeKey]}&nbsp;
-                          {standing.standing}
-                        </div>
-                      )
-                    )}
-                    {quest.finish_rewards.craftUnlock.map((craft, rIndex) => (
-                      <div
-                        key={`${rIndex}-craftUnlock-${quest.id}`}
-                        className="font-bold text-base p-[1px]"
-                      >
-                        {craft.rewardItems.map((crReward, crIndex) => (
-                          <span key={`${crIndex}-crReward-${quest.id}`}>
-                            * {questI18N.workbenchLevel[localeKey]}&nbsp;
-                            {craft.level} &nbsp;
-                            {crReward.item[getOtherLocalizedKey(locale)]}&nbsp;
-                            {questI18N.craftUnlock[localeKey]}
-                          </span>
-                        ))}
-                      </div>
-                    ))}
-                  </TableCell>
-                  <TableCell>
-                    <span
-                      className={`text-lg flex justify-center items-center font-bold`}
+          {questList.map((quest) => (
+            <TableRow key={quest.id} className="hover:bg-NeutralGray">
+              <TableCell>
+                <Link href={`/quest/detail/${quest.url_mapping}`}>
+                  <span className="text-base font-bold text-Orange hover:text-Beige flex text-center flex items-center justify-center">
+                    {quest.name[localeKey]}
+                  </span>
+                </Link>
+              </TableCell>
+              <TableCell>
+                {quest.objectives &&
+                  quest.objectives.map((obj, oIndex) => (
+                    <div
+                      className="font-bold text-base p-[1px]"
+                      key={`${oIndex}-objectives-${quest.id}`}
                     >
-                      {quest.kappa_required ? (
-                        <SquareCheckBig
-                          color={ALL_COLOR.ScreaminGreen}
-                          strokeWidth={3}
-                          size={23}
-                        />
-                      ) : (
-                        <SquareX
-                          color={ALL_COLOR.Red}
-                          strokeWidth={3}
-                          size={25}
-                        />
+                      * {obj[getDescriptionLocaleKey(locale)]}
+                      {obj.type === "shoot" && (
+                        <span className="ml-2 whitespace-nowrap">
+                          [
+                          <Skull
+                            className="inline-block w-4 h-4"
+                            color={ALL_COLOR.Red}
+                            strokeWidth={3}
+                          />
+                          x&nbsp;{obj.count}]
+                        </span>
                       )}
-                    </span>
-                  </TableCell>
-                </TableRow>
-              ))
-          )}
+                    </div>
+                  ))}
+              </TableCell>
+              <TableCell>
+                {quest.finish_rewards.items.map((rewards, rIndex) => (
+                  <div
+                    key={`${rIndex}-rewards-${quest.id}`}
+                    className="font-bold text-base p-[1px]"
+                  >
+                    * {rewards.item[getOtherLocalizedKey(locale)]} x&nbsp;
+                    {rewards.quantity}
+                  </div>
+                ))}
+                {quest.finish_rewards.offerUnlock.map((offer, rIndex) => (
+                  <div
+                    key={`${rIndex}-offerUnlock-${quest.id}`}
+                    className="font-bold text-base p-[1px]"
+                  >
+                    * {offer.trader[getOtherLocalizedKey(locale)]}&nbsp;
+                    {offer.item[getOtherLocalizedKey(locale)]}&nbsp;
+                    {questI18N.purchaseUnlock[localeKey]}
+                  </div>
+                ))}
+                {quest.finish_rewards.traderStanding.map((standing, rIndex) => (
+                  <div
+                    key={`${rIndex}-traderStanding-${quest.id}`}
+                    className="font-bold text-base p-[1px]"
+                  >
+                    * {standing.trader[getOtherLocalizedKey(locale)]}
+                    &nbsp;{questI18N.standing[localeKey]}&nbsp;
+                    {standing.standing}
+                  </div>
+                ))}
+                {quest.finish_rewards.craftUnlock.map((craft, rIndex) => (
+                  <div
+                    key={`${rIndex}-craftUnlock-${quest.id}`}
+                    className="font-bold text-base p-[1px]"
+                  >
+                    {craft.rewardItems.map((crReward, crIndex) => (
+                      <span key={`${crIndex}-crReward-${quest.id}`}>
+                        * {questI18N.workbenchLevel[localeKey]}&nbsp;
+                        {craft.level} &nbsp;
+                        {crReward.item[getOtherLocalizedKey(locale)]}&nbsp;
+                        {questI18N.craftUnlock[localeKey]}
+                      </span>
+                    ))}
+                  </div>
+                ))}
+              </TableCell>
+              <TableCell>
+                <span
+                  className={`text-lg flex justify-center items-center font-bold`}
+                >
+                  {quest.kappa_required ? (
+                    <SquareCheckBig
+                      color={ALL_COLOR.ScreaminGreen}
+                      strokeWidth={3}
+                      size={23}
+                    />
+                  ) : (
+                    <SquareX color={ALL_COLOR.Red} strokeWidth={3} size={25} />
+                  )}
+                </span>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
