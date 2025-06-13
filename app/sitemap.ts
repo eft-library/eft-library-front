d10ff0e24bd5179f46fedfbfd2c3ecf926fd8379
poeynus <poeynus@gmail.com>
2025-06-14 07:32:46 +0900

    sitemap 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,24 @@
 import { API_ENDPOINTS } from "@/lib/config/endpoint";
 
+interface Sitemap {
+  id: number;
+  update_time: string;
+  create_date: string;
+  link: string;
+  change_freq: string;
+  priority: number;
+}
+
 export default async function sitemap() {
   const getAllPosts = await fetch(API_ENDPOINTS.GET_ALL_SITEMAP);
   const allPosts = await getAllPosts.json();
 
-  const posts = allPosts.data.map((post: string) => {
+  const posts = allPosts.data.map((post: Sitemap) => {
     return {
-      url: post,
-      lastModified: new Date().toISOString().split("T")[0],
+      url: post.link,
+      lastModified: post.update_time,
+      changefreq: post.change_freq,
+      priority: post.priority,
     };
   });
 
