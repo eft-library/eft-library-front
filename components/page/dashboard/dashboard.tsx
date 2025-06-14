d4d82ec7a56524b33513dbfe12634bf1300c9ec3
poeynus <poeynus@gmail.com>
2025-06-14 19:57:00 +0900

    Dashboard 문구 변경

--- components/page/dashboard/dashboard.tsx
+++ components/page/dashboard/dashboard.tsx
@@ -4,7 +4,7 @@ import DashboardClent from "./data/dashboardClient";
 export default function Dashboard() {
   return (
     <ContentsWrapper>
-      <h1 className="text-white text-4xl font-bold text-center">Dashboard</h1>
+      <h1 className="text-white text-4xl font-bold text-center">사이트 통계</h1>
       <DashboardClent />
     </ContentsWrapper>
   );
