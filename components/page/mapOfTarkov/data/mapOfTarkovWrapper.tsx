f8d004ae22528b30d1aebd3f50b69de9cedbee71
poeynus <poeynus@gmail.com>
2025-06-21 11:11:25 +0900

    내위치찾기 마지막 수정

--- components/page/mapOfTarkov/data/mapOfTarkovWrapper.tsx
+++ components/page/mapOfTarkov/data/mapOfTarkovWrapper.tsx
@@ -5,19 +5,27 @@ import MapOfTarkovClient from "./mapOfTarkovClient";
 import MapOfTarkovSelectorClient from "./mapOfTarkovSelectorClient";
 import { useParams } from "next/navigation";
 import { useState } from "react";
+import AdBanner from "@/components/custom/adsense/adBanner";
 
 export default function MapOfTarkovWrapper({ mapData }: MapOfTarkovWrapper) {
   const param = useParams<{ id: string }>();
   const [imageSelect, setImageSelect] = useState<string>(param.id);
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center gap-2">
+      <div className="w-[1200px]">
+        <AdBanner
+          dataAdFormat={"auto"}
+          dataFullWidthResponsive={true}
+          dataAdSlot="2690838054"
+        />
+      </div>
       <MapOfTarkovSelectorClient
         setImageSelect={setImageSelect}
         imageSelect={imageSelect}
         mapData={mapData}
       />
       <MapOfTarkovClient mapData={mapData} imageSelect={imageSelect} />
-    </>
+    </div>
   );
 }
