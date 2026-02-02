8fed64e0e57b8d872f798f0f81512a1a89559eaa
poeynus <poeynus@gmail.com>
2026-02-02 19:33:24 +0900

    metadata 에러 수정

--- app/story/[id]/page.tsx
+++ app/story/[id]/page.tsx
@@ -20,20 +20,20 @@ export async function generateMetadata({
   );
 
   const res = product.data;
-
+  console.log(res);
   return {
-    title: `타르코프 스토리 ${res.name.ko} - EFT Library`,
-    description: `Escape from Tarkov(타르코프)의 플레이 스토리를 흐름 중심으로 정리한 가이드입니다. ${res.name.ko} 선행 조건, 목표, 진행 가이드를 통해 스토리 분기와 전체 루트를 한눈에 파악할 수 있습니다.`,
+    title: `타르코프 스토리 ${res.detail.name.ko} - EFT Library`,
+    description: `Escape from Tarkov(타르코프)의 플레이 스토리를 흐름 중심으로 정리한 가이드입니다. ${res.detail.name.ko} 선행 조건, 목표, 진행 가이드를 통해 스토리 분기와 전체 루트를 한눈에 파악할 수 있습니다.`,
     openGraph: {
-      title: `타르코프 스토리 ${res.name.ko} - EFT Library`,
-      description: `Escape from Tarkov(타르코프)의 플레이 스토리를 흐름 중심으로 정리한 가이드입니다. ${res.name.ko} 선행 조건, 목표, 진행 가이드를 통해 스토리 분기와 전체 루트를 한눈에 파악할 수 있습니다.`,
+      title: `타르코프 스토리 ${res.detail.name.ko} - EFT Library`,
+      description: `Escape from Tarkov(타르코프)의 플레이 스토리를 흐름 중심으로 정리한 가이드입니다. ${res.detail.name.ko} 선행 조건, 목표, 진행 가이드를 통해 스토리 분기와 전체 루트를 한눈에 파악할 수 있습니다.`,
       images: [],
       url: `https://eftlibrary.com/story/${id}`,
       siteName: "EFT Library",
     },
     twitter: {
-      title: `타르코프 스토리 ${res.name.ko} - EFT Library`,
-      description: `Escape from Tarkov(타르코프)의 플레이 스토리를 흐름 중심으로 정리한 가이드입니다. ${res.name.ko} 선행 조건, 목표, 진행 가이드를 통해 스토리 분기와 전체 루트를 한눈에 파악할 수 있습니다.`,
+      title: `타르코프 스토리 ${res.detail.name.ko} - EFT Library`,
+      description: `Escape from Tarkov(타르코프)의 플레이 스토리를 흐름 중심으로 정리한 가이드입니다. ${res.detail.name.ko} 선행 조건, 목표, 진행 가이드를 통해 스토리 분기와 전체 루트를 한눈에 파악할 수 있습니다.`,
       images: [],
     },
   };
